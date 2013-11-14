a1a0413a160f00e565581b9951179ad0ccba5a49
Jacob Ostrem <jacob-ostrem@uiowa.edu>
2013-11-14 14:30:25 -0600

    continued to update site

--- library.master.vb
+++ library.master.vb
@@ -1,5 +1,10 @@
 ﻿
 Partial Class library
     Inherits System.Web.UI.MasterPage
+
+
+    Protected Sub Label1_Load(sender As Object, e As EventArgs) Handles Label1.Load
+        Label1.Text = DateTime.Now.ToLongDateString
+    End Sub
 End Class
 
